652e69078c46c0e18d92b0a20cbd100e1dc7c323
Bryan Cable <bryancable@Bryans-MacBook-Pro.local>
2018-05-14 18:32:05 -0700

    almost done...

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,37 @@
 
-window.onload = function () {
+
     // variables
     let form = document.getElementById("form");
     let input = document.getElementById("input");
     let btn = document.getElementById("btn");
     let list = document.getElementById("list");
 
+    
+
     // event listener for btn
     btn.addEventListener("click", function() {
-        let value = ('item').value
-        conole.log(value);
         if(input.value === "") {
             alert("You must enter a task")
         }
+        else {
+        let value = input.value
+        let newLi =
+        `
+        <li>
+            <input type="checkbox">
+            <p>${value}</p>
+            <i class="edit-icon"></i>
+            <i class="fa-trash-alt"></i>
+        </li>
+        `
+        var li = document.createElement("li");
+        li.innerHTML = newLi;
+        // li.appendChild(document.createTextNode(newLi));
+        list.appendChild(li);
+        }
         
-        });
+        
+        })
 
     // add todo item to list
-    
-}
+
